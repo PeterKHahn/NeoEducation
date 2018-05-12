30e34f50574519e97a7206a36bbfba35a5a2914a
Peter Hahn <peterkunho@gmail.com>
2018-05-12 14:10:07 -0400

    Everything is now fixed, textboxes are now focused and expand correctly

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import TextareaAutosize from 'react-autosize-textarea'
+
+
 
 class CardSet extends React.Component {
     
@@ -8,10 +11,7 @@ class CardSet extends React.Component {
             cards : [<CardContent/>]
         }
         this.addCard = this.addCard.bind(this)
-        this.dragOver.bind(this)
-        this.dragStart.bind(this)
-        this.dragEnd.bind(this)
-
+       
        
     }
 
@@ -26,69 +26,44 @@ class CardSet extends React.Component {
         })
     }
 
-
-    dragStart(event) {
-        console.log('dragstart')
-        event.dataTransfer.effectAllowed = 'move';
-
-    }
-
-    dragEnd(event) {
-        console.log('dragend')
-    }
-
-    dragOver(event) {
-        event.preventDefault()
-        console.log('dragOver')
-    }
-
     render(){
         let ls = this.state.cards;
         let res = ls.map((currElement, index) => {
             return <Card 
                     index={index} 
                     content={currElement}
-                    draggable='true'
-                    onDragStart={this.dragStart}
-                    onDragEnd={this.dragEnd}/>
+                    />
         })
 
         console.log(res)
 
         return  <div className='card-set'>
                     <CardSetInfoBox/>
-                    <div onDragOver={this.dragOver}>
+                    <div >
                         {res}
                     </div>
                 </div>
     }
 }
 
-class CardSetInfo extends React.Component {
+class CardSetInfo extends Component {
     render() {
         return(
             <div className='cardset-info'>
-                <textarea tabindex='5' 
-                    className='text card-set-title' 
-                    placeholder='Title your Flash Set'>
-                </textarea>
-                <textarea className='text' 
-                    contenteditable="true"
-                    tabindex='5' 
-                    placeholder='Subject...'>
-                </textarea>
+
+                <TextareaAutosize className='text-area' placeholder='Title your Flash Set'/>
+                <TextareaAutosize className='text-area' placeholder='Subject...'/>
+
             </div>
         )
     }
 }
 
-class Card extends React.Component {
+class Card extends Component {
 
     render() {
         return(
-            <div className="card" 
-                onDragStart={this.props.onDragStart}
-                onDragEnd={this.props.onDragEnd}>
+            <div className="card" >
                 <CardCounter index={this.props.index}/>
                 {this.props.content}
             </div>
@@ -98,12 +73,8 @@ class Card extends React.Component {
 
 class CardContent extends React.Component {
 
-    componentDidMount() {
-        console.log(this.ref.current)
-        this.ref.current.focus()
-    }
 
-    
+     
     render(){
         this.ref = React.createRef();
         return(
@@ -129,7 +100,6 @@ class Definition extends React.Component {
         super(props)
         this.state = {
             definition : "",
-            height : 20
         }
         this.handleChange = this.handleChange.bind(this)
         this.textRef = React.createRef();
@@ -139,39 +109,22 @@ class Definition extends React.Component {
     }
 
     handleChange(event) {
-        this.setState({
-            definition : event.target.value, 
-            height : 0
-        })
-        console.log(this.textRef)
-        console.log(this.textRef.current.scrollHeight)
 
         this.setState({
             definition : event.target.value, 
-            height : this.textRef.current.scrollHeight
         })
 
 
     }
 
-      /*$(".text").on('input', function() {
-        console.log('fuck')
-        $(this).css('height','auto');
-        $(this).height(this.scrollHeight);
-    })*/
-
     
     render() {
         return(
             <div className='definition'>
-                <textarea className='text' 
-                    placeholder="Definition..."
-                    ref={this.textRef}
-                    tabindex='5' 
-                    rows={this.state.rows}
-                    style={{height: this.state.height}}
-                    onChange={this.handleChange} 
-                    value={this.state.definition}></textarea>
+                <TextareaAutosize
+                    className='text-area' 
+                    placeholder='Definition...'
+                    tabindex={5}/>
             </div>
         )
     }
@@ -186,6 +139,15 @@ class Term extends React.Component {
         this.textRef = React.createRef();
 
 
+        this.childText = null; 
+        this.testRef = element => {
+          
+            this.childText = element
+
+            this.childText.focus()
+            
+        }
+
 
     }
 
@@ -195,19 +157,19 @@ class Term extends React.Component {
         this.setState({term : event.target.value})
     }
 
-    focus() {
-        this.textRef.current.focus()
-    }
+
 
     render() {
+        console.log(this.props)
         return(
             <div className='term'>
-                <textarea className='text' 
-                    ref = {this.textRef}
-                    tabindex='5' 
-                    placeholder="Term..." 
-                    autoFocus
-                    onChange={this.handleChange} value={this.state.term}></textarea>
+                <TextareaAutosize 
+                    className='text-area' 
+                    innerRef = {this.testRef} 
+                    placeholder='Term...'
+                    tabindex={5}/>
+
+                
             </div>
         )
     }
