43d05a26d9321694241ba0983cf0080315cbe415
Peter Hahn <peterkunho@gmail.com>
2018-07-12 17:18:09 -0400

    Got the priority bar working

--- frontend/src/viewer/CardViewer.jsx
+++ frontend/src/viewer/CardViewer.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'; 
 import {connect} from 'react-redux';
+import { Range } from 'rc-slider';
 
 import PriorityBar from 'viewer/PriorityBar.jsx'
+import 'rc-slider/assets/index.css';
+
+
 
 
 class CardSetViewer extends Component {
@@ -10,11 +14,21 @@ class CardSetViewer extends Component {
         super(props)
         this.state = {
             priorityStart: 1,
-            priorityEnd: 3,
+            priorityEnd: 4,
         }
+        this.updateRange = this.updateRange.bind(this)
 
     }
 
+
+    updateRange(range) {
+        this.setState({
+            priorityStart: range[0],
+            priorityEnd: range[1]
+        })
+
+    }
+    
    
 
     render() {
@@ -42,9 +56,11 @@ class CardSetViewer extends Component {
         <div>
             <h1>{this.props.title}</h1>
             <h2>{this.props.subject}</h2>
-            
             <div className="work-panel">
+
                 <div className="flash-cards-panel-container">
+                    <PriorityControl updateRange={this.updateRange}/>
+
                     <div className='flash-cards-panel'>{cards}</div>
                 </div>
 
@@ -57,6 +73,31 @@ class CardSetViewer extends Component {
 }
 
 
+class PriorityControl extends Component {
+
+
+
+
+    render() {
+        return(
+            <div>
+                <Range defaultValue={[0,4]}
+                        allowCross={false}
+                        min={1}
+                        max={4}
+                        dots={true}
+                        onAfterChange={this.props.updateRange}
+                        marks={{
+                            1 : "1",
+                            2 : "2",
+                            3 : "3", 
+                            4 : "4"
+                        
+                        }}/>
+            </div>
+        )
+    }
+}
 
 
 const Card = (props) => {
